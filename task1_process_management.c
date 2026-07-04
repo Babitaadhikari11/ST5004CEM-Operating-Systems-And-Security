41c433b8c5094a1d5d61dd346829f63d83f9b012
babitaadhikari11 <babeetaadhikari123@gmail.com>
2026-07-04 14:43:57 +0000

    Add POSIX thread creation and management

--- task1_process_management.c
+++ task1_process_management.c
@@ -3,9 +3,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <pthread.h>
+#define THREADS 3
 /* parent and child process creation */
 void process_demo(){
-	printf("Process Creation");
+	printf("Process Creation\n");
 	printf("Parent PID : %d\n",getpid());
 	/* preventing duplicate output after fork */
 	fflush(stdout);
@@ -25,8 +27,40 @@ void process_demo(){
 	wait(NULL);
 	printf("Parent waited for child process.\n");
 }
+/*function performed by each worker thread*/
+void *worker_task(void *arg){
+	int id = *(int *)arg;
+	printf("Thread %d is running.\n", id);
+	return NULL;
+}
+/*three worker thread*/
+void run_thread(){
+	pthread_t threads[THREADS];
+	int id[THREADS];
+	printf("thread management\n");
+	/*three threads creating*/
+	 for (int i = 0; i < THREADS; i++) {
+        id[i] = i + 1;
+
+        pthread_create(
+            &threads[i],
+            NULL,
+            worker_task,
+            &id[i]
+        );
+    }
+
+    /* waiting for all threads to finish via pthread_join */
+    for (int i = 0; i < THREADS; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    printf("all worker threads completed.\n");
+}
+
 /* main function*/
 int main(){
 	process_demo();
+	run_thread();
 	return 0;
 }
