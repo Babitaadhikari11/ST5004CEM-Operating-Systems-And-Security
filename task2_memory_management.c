f734febbc6e3d4f3596159c73524c1fcc82884ce
babitaadhikari11 <babeetaadhikari123@gmail.com>
2026-07-14 05:28:32 +0000

    2: Add FIFO page replacement

--- task2_memory_management.c
+++ task2_memory_management.c
@@ -1,11 +1,65 @@
 /*task2 hospital emergency dashboard paging system*/
 #include <stdio.h>
 #define TOTAL_PAGES 8
+#define FRAMES 3
+#define REQUESTS 12
 /* stores hospital dashboard page details */
 struct Page {
 int pageNumber;
 char pageName[40];
 };
+/*checks if requested page is already in memory or not*/
+int find_page(int frames[], int page){
+	for(int i=0;i<FRAMES;i++){
+		if(frames[i] == page){ /*if requested page is found, it returns it position*/
+			return i;
+}
+}
+	return -1; /*else not found*/
+}
+/*displaying current memory frame*/
+void show_frames(int frames[]){
+	printf("memory frames: ");
+	for(int i =0;i<FRAMES;i++){
+		if(frames[i]==-1){
+			printf("[empty] "); /*if frame has no page*/
+}else{
+			printf("[page %d] ",frames[i]);
+}
+}
+	printf("\n");
+}
+/*FIFO replaces the pgae that entered memory first*/
+void fifo_simulation(int requests[]){
+	int frames[FRAMES]; /*stores currentpage in physixal memory*/
+	int nextReplace =0; /*tells which frame should be replaced next*/
+	int hits =0; /*request page in memory*/
+	int pageFaults=0; /*request page not in memory*/
+	for(int i=0;i<FRAMES;i++){
+		frames[i]=-1;
+}
+	printf("FIFO Page Replacement");
+	printf("Physical Memory Frames: %d\n",FRAMES);
+	for(int i=0;i<REQUESTS;i++){ /*this loops processes all 12 hospital page requests*/
+		int page=requests[i];
+		printf("\nrequest %d: hospital page %d\n",i+1,page);
+		if(find_page(frames,page)!=-1){
+			hits++;
+			printf("result: hit,page already in memory.\n");
+}else{
+			pageFaults++;
+			printf("result: page fault, loading page.\n");
+			frames[nextReplace]=page;
+			nextReplace=(nextReplace+1)%FRAMES;
+}
+		show_frames(frames);
+}
+printf("\n fifo result\n");
+printf("total requests: %d\n", REQUESTS);
+printf("hits: %d\n", hits);
+printf("hit ratio: %.2f\n", (float)hits / REQUESTS);
+printf("miss ratio: %.2f\n", (float)pageFaults / REQUESTS);
+}
 int main(){
 int pageSize;
 int logicalAddress;
@@ -34,5 +88,10 @@ pages[pageNumber].pageName);
 } else {
 printf("invalid page number.\n");
 }
+/*fifo page request sequence*/
+int requests[REQUESTS]={
+1,2,3,3,4,5,2,3,6,2,7,3
+};
+fifo_simulation(requests);
 return 0;
 }
