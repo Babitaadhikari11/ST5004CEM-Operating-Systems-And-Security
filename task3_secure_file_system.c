173608ff5f7a700a0fd04c6c24af60e6865f0294
babitaadhikari11 <babeetaadhikari123@gmail.com>
2026-07-15 17:31:39 +0000

    1: Add user authentication

---
+++ task3_secure_file_system.c
@@ -0,0 +1,50 @@
+/*task 3 hospital patient file management system*/
+#include <stdio.h>
+#include <string.h>
+#define MAX_USERS 3
+/* stores login details and hospital user role */
+struct User {
+        char username[30];
+        char password[30];
+        char role[20];
+};
+/* checks username and password entered by user */
+int login(struct User users[], struct User *currentUser) {
+        char username[30];
+        char password[30];
+      printf(" hospital secure file system login \n");
+	printf("username: ");
+        scanf("%s", username);
+	printf("password: ");
+        scanf("%s", password);
+/*check login details*/
+	for(int i= 0;i< MAX_USERS;i++) {
+/*strcmp() is used for comparing two null terminated strings */
+                if (strcmp(username,users[i].username)==0 &&
+                    strcmp(password,users[i].password)==0){
+/*successfull Login*/
+			*currentUser= users[i];
+                        printf("\nlogin successful.\n");
+                        printf("logged in as: %s\n", currentUser->role);
+                        return 1; /*return 1 if logged in*/
+          }
+        }
+      printf("\nlogin failed. invalid username or password.\n");
+        return 0;
+}
+int main(){
+        struct User users[MAX_USERS] = {{"admin", "admin123", "admin"},
+                {"doctor", "doctor123", "doctor"},
+                {"nurse", "nurse123", "nurse"}
+        };
+        struct User currentUser;
+        int loggedIn;
+        loggedIn= login(users, &currentUser);
+	/*stop program if login fails*/
+        if (loggedIn== 0){
+
+                return 0;
+        }
+        printf("\nwelcome to hospital patient file management system.\n");
+        return 0;
+}
